9dc7921d3c6cc8136b36b5a299ace13912b8d47c
srujithch <srujith.v2@gmail.com>
2020-05-23 23:14:17 +0530

    Added react native elements

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Button, ThemeProvider } from "react-native-elements";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello world</Text>
+      <ThemeProvider>
+        <Button title="Hey!" />
+      </ThemeProvider>
     </View>
   );
 }
